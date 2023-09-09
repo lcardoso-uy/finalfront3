3577d118c4e94140c4a3bfd30056be531c7ecfa6
lcardoso-uy <lcardosogx@gmail.com>
2023-09-09 09:27:20 -0300

    Version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import ThemeContext, {themes} from './context';
 
-import Footer from "./Components/Footer";
+import React from 'react';
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
+import Layout from './Components/Layout';
+import { useState } from "react";
 
 function App() {
+
+  const [theme, setTheme] = useState(themes.light);
+
+  const handleChangeTheme = () => {
+    theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
+  };
+
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
+        <Layout>
+          <Navbar />
+          <Footer />
+        </Layout>
+      </ThemeContext.Provider>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from "react";
+import ThemeContext from '../context';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
+  const{theme, handleChangeTheme} = useContext(ThemeContext);
+
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <button onClick={handleChangeTheme}
+        style={{background: theme.background, color: theme.font}}>Change theme</button>
     </nav>
   )
 }
