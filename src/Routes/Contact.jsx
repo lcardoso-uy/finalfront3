3bbaf159f58f9a6d566114f1209a5f965f0a9a33
lcardoso-uy <lcardosogx@gmail.com>
2023-09-11 21:24:04 -0300

    Version 8 - formulario y validaciones

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ThemeContext from "../context";
 import Form from '../Components/Form';
 
@@ -7,13 +7,24 @@ const Contact = () => {
 
   const { theme } = useContext(ThemeContext);
 
+  const [usuario, setUsuario] = useState({});
+
+  const addUsuario = (nombre, email, estadoValidacion, resultado) => {
+    setUsuario({nombre, email, estadoValidacion, resultado})
+  }
+
   return (
     <div style={{ background: theme.background, color: theme.font }}>
       <h2>Want to know more?</h2>
       <p>Send us your questions and we will contact you</p>
-      <Form/>
+      <Form onAddUsuario={addUsuario}/>    
+      <div>{usuario.resultado}</div>
     </div>
   )
 }
 
 export default Contact;
+
+/*○ Una vez enviado el formulario, deberán mostrar un mensaje de éxito que
+contenga el siguiente formato: “Gracias [nombre del usuario], te
+contactaremos cuando antes vía mail”.*/
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,51 @@
 import React from "react";
+import { useState } from "react";
 
 
-const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+const Form = ({ onAddUsuario }) => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+
+  const validarNombre = (nombre) => {
+    return (nombre[0] !== ' ' && nombre.length > 5);
+  }
+
+  const validarEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const esValido = validarNombre(nombre) && validarEmail(email);
+    const estadoValidacion = esValido ? "S" : "N";
+
+    const resultado = estadoValidacion === "S" ? `Gracias ${nombre}, te contactaremos cuanto antes vía mail` : "Por favor verifique su información nuevamente";
+
+    onAddUsuario(nombre, email, estadoValidacion, resultado);
+
+    setNombre("");
+    setEmail("");
+  };
+
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Ingrese su nombre " value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        <input type="text" placeholder="Ingrese el email " value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit">Registrar</button>
       </form>
     </div>
   );
 };
 
 export default Form;
+
+
+/*Nombre completo (con longitud mayor a 5).
+■ Email (con formato correcto de email).
+○ En caso de haber un error, mostrar el siguiente mensaje de error: “Por favor
+verifique su información nuevamente”.*/
